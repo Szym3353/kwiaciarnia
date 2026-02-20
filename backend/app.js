2789d735873d6fde19cb7c7a4ebfee3940ca0326
szymg2001 <szymon.grabowiec2001@gmail.com>
2026-02-20 01:03:47 +0100

    Fix backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const cors = require("cors"); // Zainstaluj: npm install cors
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
